ee5e8e785fe185628be089fddb4080b58acdb112
yamaso-burger <apple_1228_ringo@yahoo.co.jp>
2022-08-22 11:45:03 +0900

    install body-parser

--- app.js
+++ app.js
@@ -5,28 +5,34 @@ const https = require("https");
 const app = express();
 
 app.get("/", (req, res)=> {
-
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Tokyo,jp&units=metric&appid=ee209b530165edc961c1f49e89598afa";
-    https.get(url, function(response){
-        console.log(response.statusCode);
-        response.on("data", function(data){
-            const weatherData = JSON.parse(data);
-            const temp = weatherData.main.temp;
-            const description = weatherData.weather[0].description;
-            const icon = "<img src='http://openweathermap.org/img/wn/" + weatherData.weather[0].icon + "@2x.png'>";
-            console.log(temp + "degree, " + description);
-
-            //If you want to do additional res.send(), you can use res.write
-            res.write("<h1>The weather is currently " + description + "</h1>");
-            res.write(icon);
-            res.write("<h1>The temperature in Osaka is " + temp + " degrees Celcius.</h1>");
-            res.send();
-        });
-    });
-
-
+    res.sendFile(__dirname + "/index.html");
     // res.send("Server is running!");
 });
+
+app.post("/", (req, res)=> {
+    console.log("Post received");
+})
+
+// const query = "Osaka";
+//     const apiKey = "ee209b530165edc961c1f49e89598afa";
+//     const unit = "metric";
+//     const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + ",jp&units=" + unit + "&appid=" + apiKey;
+//     https.get(url, function(response){
+//         console.log(response.statusCode);
+//         response.on("data", function(data){
+//             const weatherData = JSON.parse(data);
+//             const temp = weatherData.main.temp;
+//             const description = weatherData.weather[0].description;
+//             const icon = "<img src='http://openweathermap.org/img/wn/" + weatherData.weather[0].icon + "@2x.png'>";
+//             console.log(temp + "degree, " + description);
+
+//             //If you want to do additional res.send(), you can use res.write
+//             res.write("<h1>The weather is currently " + description + "</h1>");
+//             res.write(icon);
+//             res.write("<h1>The temperature in Osaka is " + temp + " degrees Celcius.</h1>");
+//             res.send();
+//         });
+//     });
 
 app.listen(port, function() {
     console.log("Server is running on port " + port + ".");
